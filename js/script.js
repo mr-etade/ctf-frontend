1e85ac54a84532db3a40b3493dfb61dcf7b2fee6
Eremas Tade Junior <etade@wpu.ac.pg>
2025-05-31 21:42:07 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -236,7 +236,7 @@ function initProgressChart(data, teamName) {
       text: `Your Progress: ${teamName}`,
       align: 'center',
       verticalAlign: 'bottom',
-      y: 50,
+      y: 10,
       style: {
         fontSize: '14px',
         fontWeight: 'bold'
@@ -328,7 +328,7 @@ function initDifficultyChart(data, teamName) {
       text: `Your Difficulty Breakdown: ${teamName}`,
       align: 'center',
       verticalAlign: 'bottom',
-      y: 50,
+      y: 10,
       style: {
         fontSize: '14px',
         fontWeight: 'bold'
